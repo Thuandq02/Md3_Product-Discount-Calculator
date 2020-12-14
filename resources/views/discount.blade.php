3aca664ec66ea212ad191176809d987a2a55931f
dqthua <duongquangthuan2002@gmail.com>
2020-12-14 16:53:08 +0700

    ll

---
+++ resources/views/discount.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Product Discount Caculstor</h1>
+<form action="/show" method="post">
+    @csrf
+    <p>
+        <b>Product Description : </b>
+        <input type="text" name="Product Description">
+    </p>
+    <p>
+        <b>List Price : </b>
+        <input type="text" name="List Price">
+    </p>
+    <p>
+        <b>Discount Percent : </b>
+        <input type="text" name="Discount Percent">
+    </p>
+    <p>
+        <button type="submit">Calculate Discount</button>
+    </p>
+</form>
+
+</body>
+</html>
